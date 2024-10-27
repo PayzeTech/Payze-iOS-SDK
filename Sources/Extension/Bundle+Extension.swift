1086767d32f9b9ce5d66ab7efab244a95b0b649d
Giga Khizanishvili <khizanag@gmail.com>
2024-10-28 00:51:15 +0400

    Remove 2 from module2

--- Sources/Extension/Bundle+Extension.swift
+++ Sources/Extension/Bundle+Extension.swift
@@ -11,7 +11,7 @@ extension Bundle {
 #if SWIFT_PACKAGE
 
 #else
-    static var module2: Bundle {
+    static var module: Bundle {
         let bundle = Bundle(for: Configuration.self)
         let bundleURL = bundle.resourceURL?.appendingPathComponent("PayzeResources.bundle")
         let resourceBundle = Bundle(url: bundleURL!)
